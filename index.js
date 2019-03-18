4d21c2cbdff059fc04f4218dbb5ac1c1711615e0
mazenswar <mazenswar@protonmail.com>
2019-03-18 12:30:21 -0400

    iterators

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+
+const superbowlWin = (arrOfObjs) => {
+  let win = arrOfObjs.find( obj => obj.result === "W");
+  return (win ? win.year : undefined);
+}
